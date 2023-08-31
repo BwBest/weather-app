8f261e5b9fa42101cd9593cbaa182c5a5c122b9c
BwBest <raffine49@gmail.com>
2023-09-01 00:49:46 +0300

    Refactor code and add initalize function

--- src/index.js
+++ src/index.js
@@ -4,18 +4,29 @@ import * as ui from './ui.js';
 let selectedCity = 'auto:ip';
 const forecastArray = [];
 
+function initalize() {
+  console.log('initalizing..');
+  const data = getData().then((value) => {
+    console.log(value);
+    ui.updateTemperature(value.currentTemp);
+    ui.updateLocation(value.cityName, value.countryName);
+  });
+}
+
 async function getData() {
   const data = await fetch(
     `https://api.weatherapi.com/v1/current.json?key=bb0f91bbe7304e7286c165412233008&q=${selectedCity}`,
     { mode: 'cors' }
   );
-  const responseCurrent = await data.json();
-
   const forecastData = await fetch(
     `https://api.weatherapi.com/v1/forecast.json?key=bb0f91bbe7304e7286c165412233008&q=${selectedCity}&days=7`,
     { mode: 'cors' }
   );
+
+  const responseCurrent = await data.json();
   const responseForecast = await forecastData.json();
+
+  console.log(responseCurrent);
 
   const forecastsJSON = responseForecast.forecast.forecastday;
 
@@ -24,9 +35,8 @@ async function getData() {
     forecastArray.push({
       dayName: calculateDay(fDate.getDay()),
       temperature: item.day.avgtemp_c,
-      conditionText: item.day.condition.text,
+      condition: item.day.condition.code,
     });
-
     console.log(forecastArray);
   });
 
@@ -43,4 +53,4 @@ function calculateDay(day) {
   return days[day];
 }
 
-getData();
+window.addEventListener('load', initalize);
--- src/ui.js
+++ src/ui.js
@@ -1,3 +1,8 @@
+export function updateLocation(city, country) {
+  const locText = document.querySelector('#location-text');
+  locText.textContent = `${city}, ${country}`;
+}
+
 export function updateTemperature(temp) {
   const tempElement = document.querySelector('#temperature-text');
   tempElement.textContent = `${temp}°C`;
@@ -14,28 +19,67 @@ export function updateWeatherIcon(type, iconRef = 'main') {
     size = 'fa-2x';
   }
 
-  switch (type) {
-    case 'sun':
-      weatherIcon.classList = `fa-solid ${size} fa-sun`;
-      break;
-    case 'suncloud':
-      weatherIcon.classList = `fa-solid ${size} fa-cloud-sun`;
-      break;
-    case 'cloud':
-      weatherIcon.classList = `fa-solid ${size} fa-cloud`;
-      break;
-    case 'rain':
-      weatherIcon.classList = `fa-solid ${size} fa-cloud-rain`;
-      break;
-    case 'raincloud':
-      weatherIcon.classList = `fa-solid ${size} fa-cloud-sun-rain`;
-      break;
-    case 'storm':
-      weatherIcon.classList = `fa-solid ${size} fa-cloud-showers-heavy`;
-      break;
-    case 'snow':
-      weatherIcon.classList = `fa-solid ${size} fa-snowflake`;
-      break;
+  if (type === 1000) {
+    weatherIcon.classList = `fa-solid ${size} fa-sun`;
+  }
+  if (type === 1003) {
+    weatherIcon.classList = `fa-solid ${size} fa-cloud-sun`;
+  }
+  if (
+    type === 1006 ||
+    type === 1009 ||
+    type === 1030 ||
+    type === 1135 ||
+    type === 1147
+  ) {
+    weatherIcon.classList = `fa-solid ${size} fa-cloud`;
+  }
+  if (
+    type === 1063 ||
+    type === 1150 ||
+    type === 1153 ||
+    type === 1168 ||
+    type === 1171 ||
+    type === 1189 ||
+    type === 1192 ||
+    type === 1195 ||
+    type === 1198 ||
+    type === 1201
+  ) {
+    weatherIcon.classList = `fa-solid ${size} fa-cloud-rain`;
+  }
+  if (type === 1063 || type === 1183 || type === 1186) {
+    weatherIcon.classList = `fa-solid ${size} fa-cloud-sun-rain`;
+  }
+  if (type === 1087 || type === 1240 || type === 1243 || type === 1246) {
+    weatherIcon.classList = `fa-solid ${size} fa-cloud-showers-heavy`;
+  }
+  if (
+    type === 1069 ||
+    type === 1072 ||
+    type === 1114 ||
+    type === 1117 ||
+    type === 1204 ||
+    type === 1207 ||
+    type === 1210 ||
+    type === 1213 ||
+    type === 1216 ||
+    type === 1219 ||
+    type === 1222 ||
+    type === 1225 ||
+    type === 1237 ||
+    type === 1249 ||
+    type === 1252 ||
+    type === 1255 ||
+    type === 1258 ||
+    type === 1261 ||
+    type === 1264 ||
+    type === 1273 ||
+    type === 1276 ||
+    type === 1279 ||
+    type === 1282
+  ) {
+    weatherIcon.classList = `fa-solid ${size} fa-snowflake`;
   }
 }
 
